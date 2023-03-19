6532c58a34bb99b337b28e4be3449bf3b533f9ba
Shreck Ye <ShreckYe@gmail.com>
2023-03-19 10:20:21 +0800

    Add the SCRAM client dependency in "vertx-web-kotlinx" which seems to be required after PostgreSQL is updated to 15

--- frameworks/Kotlin/vertx-web-kotlinx/build.gradle.kts
+++ frameworks/Kotlin/vertx-web-kotlinx/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-html:0.8.0")
     //implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+
+    runtimeOnly("com.ongres.scram:client:2.1")
 }
 
 tasks.withType<KotlinCompile> {

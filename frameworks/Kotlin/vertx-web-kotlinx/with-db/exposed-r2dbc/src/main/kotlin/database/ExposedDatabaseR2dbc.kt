2653377a52b2bb9ac3a8cda076eb8acd7a6ca7b9
Yongshun Ye <ShreckYe@gmail.com>
2026-02-08 13:28:07 +0800

    Remove 2 unused functions

--- frameworks/Kotlin/vertx-web-kotlinx/with-db/exposed-r2dbc/src/main/kotlin/database/ExposedDatabaseR2dbc.kt
+++ frameworks/Kotlin/vertx-web-kotlinx/with-db/exposed-r2dbc/src/main/kotlin/database/ExposedDatabaseR2dbc.kt
@@ -4,16 +4,6 @@ import org.jetbrains.exposed.v1.core.vendors.PostgreSQLDialect
 import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabase
 import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabaseConfig
 
-fun r2DbcDatabaseConnect() =
-    R2dbcDatabase.connect(connectionFactoryOptimized, R2dbcDatabaseConfig {
-        explicitDialect = PostgreSQLDialect()
-    })
-
-fun r2dbcDatabaseConnectPool(connectionPoolSize: Int) =
-    R2dbcDatabase.connect(connectionPoolOptimized(connectionPoolSize), R2dbcDatabaseConfig {
-        explicitDialect = PostgreSQLDialect()
-    })
-
 // Configuration variants for benchmarking
 fun r2dbcDatabaseConnectPoolOriginal(connectionPoolSize: Int) =
     R2dbcDatabase.connect(connectionPoolOriginal(connectionPoolSize), R2dbcDatabaseConfig {

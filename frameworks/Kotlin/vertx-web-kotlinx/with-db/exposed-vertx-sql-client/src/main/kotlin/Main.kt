fa264d9f48b6a65c31807a8e555de23e8ac1f83a
Yongshun Ye <ShreckYe@gmail.com>
2026-02-12 15:41:59 +0800

    Test a custom transaction provider with one new but closed transaction per `DatabaseClient` execution

--- frameworks/Kotlin/vertx-web-kotlinx/with-db/exposed-vertx-sql-client/src/main/kotlin/Main.kt
+++ frameworks/Kotlin/vertx-web-kotlinx/with-db/exposed-vertx-sql-client/src/main/kotlin/Main.kt
@@ -1,12 +1,29 @@
 import com.huanshankeji.exposedvertxsqlclient.ExperimentalEvscApi
-import com.huanshankeji.exposedvertxsqlclient.JdbcTransactionExposedTransactionProvider
+import com.huanshankeji.exposedvertxsqlclient.StatementPreparationExposedTransactionProvider
 import com.huanshankeji.exposedvertxsqlclient.postgresql.exposed.exposedDatabaseConnectPostgresql
 import database.connectionConfig
+import org.jetbrains.exposed.v1.core.InternalApi
+import org.jetbrains.exposed.v1.core.Transaction
+import org.jetbrains.exposed.v1.core.transactions.withThreadLocalTransaction
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import java.sql.Connection
 
-@OptIn(ExperimentalEvscApi::class)
+@OptIn(ExperimentalEvscApi::class, InternalApi::class)
 suspend fun main() =
     commonRunVertxServer(
         "Vert.x-Web Kotlinx with Exposed Vert.x SQL Client (and PostgreSQL)",
-        { JdbcTransactionExposedTransactionProvider(connectionConfig.exposedDatabaseConnectPostgresql()) },
+        {
+            val database = connectionConfig.exposedDatabaseConnectPostgresql()
+            object : StatementPreparationExposedTransactionProvider {
+                override fun <T> statementPreparationExposedTransaction(block: Transaction.() -> T): T {
+                    val transaction = transaction(database, Connection.TRANSACTION_READ_UNCOMMITTED, true) { this }
+                    return withThreadLocalTransaction(transaction) { transaction.block() }
+                }
+
+                override fun <T> withExplicitOnlyStatementPreparationExposedTransaction(block: Transaction.() -> T): T {
+                    TODO("Not yet implemented")
+                }
+            }
+        },
         ::MainVerticle
     )
